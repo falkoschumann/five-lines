20f240854fe2b8035f649a1bb9b3fd26f1f44cf9
Falko Schumann <falko.schumann@muspellheim.de>
2023-10-05 21:14:43 +0200

    Inline updateTile

--- index.ts
+++ index.ts
@@ -753,13 +753,9 @@ function handleInputs() {
 function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      updateTile(x, y);
+      map[y][x].update(x, y);
     }
   }
-}
-
-function updateTile(x: number, y: number) {
-  map[y][x].update(x, y);
 }
 
 function draw() {
